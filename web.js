bb3ee76a7f4970238c989eaa66a6d528732b77c4
luca palomba <luca.paco.luca@hotmail.it>
2013-08-30 12:15:07 +0200

    payment success routed

--- web.js
+++ web.js
@@ -1,11 +1,13 @@
 // Define routes for simple SSJS web app.
 // Writes Coinbase orders to database.
-var async   = require('async')
-  , express = require('express')
-  , fs      = require('fs')
-  , http    = require('http')
-  , https   = require('https')
-  , db      = require('./models');
+var async = require('async')
+        , express = require('express')
+        , fs = require('fs')
+        , http = require('http')
+        , https = require('https')
+        , db = require('./models')
+        , qs = require('querystring')
+        , url = require('url');
 
 var app = express();
 app.set('views', __dirname + '/views');
@@ -15,31 +17,45 @@ app.set('port', process.env.PORT || 8080);
 app.use(express.static(__dirname + '/public')); // images
 // Render homepage (note trailing slash): example.com/
 app.get('/', function(request, response) {
-  var data = fs.readFileSync('index.html').toString();
-  response.send(data);
+    var data = fs.readFileSync('index.html').toString();
+    response.send(data);
 });
 
 // Render example.com/orders
 app.get('/orders', function(request, response) {
-  global.db.Order.findAll().success(function(orders) {
-    var orders_json = [];
-    orders.forEach(function(order) {
-      orders_json.push({id: order.coinbase_id, amount: order.amount, time: order.time});
+    global.db.Order.findAll().success(function(orders) {
+        var orders_json = [];
+        orders.forEach(function(order) {
+            orders_json.push({id: order.coinbase_id, amount: order.amount, time: order.time});
+        });
+        // Uses views/orders.ejs
+        response.render("orders", {orders: orders_json});
+    }).error(function(err) {
+        console.log(err);
+        response.send("error retrieving orders");
     });
-    // Uses views/orders.ejs
-    response.render("orders", {orders: orders_json});
-  }).error(function(err) {
-    console.log(err);
-    response.send("error retrieving orders");
-  });
 });
 
-app.get('/paypal_failed',function(request,response){
+app.get('/paypal_failed', function(request, response) {
 
-//failed payment, return to home
- var data = fs.readFileSync('index.html').toString();
-  response.send(data);
-  
+//failed payment, intercept event, redirect to:
+
+    response.writeHead(301, {'Location': '/'});
+    response.end();
+
+});
+
+app.get('/payment_success', function(request, response) {
+
+ //TO-DO control if it's really a paypal payment
+
+    //semplified express edition
+    var payment_value = request.query.order;
+    global.db.Order.insert({coinbase_id: "Paypal Donator", amount: payment_value, time: Date.now().toString()})
+
+    response.writeHead(301, {'Location': '/'});
+    response.end();
+
 });
 
 
@@ -47,77 +63,77 @@ app.get('/paypal_failed',function(request,response){
 
 // Hit this URL while on example.com/orders to refresh
 app.get('/refresh_orders', function(request, response) {
-  https.get("https://coinbase.com/api/v1/orders?api_key=" + process.env.COINBASE_API_KEY, function(res) {
-    var body = '';
+    https.get("https://coinbase.com/api/v1/orders?api_key=" + process.env.COINBASE_API_KEY, function(res) {
+        var body = '';
     res.on('data', function(chunk) {body += chunk;});
-    res.on('end', function() {
-      try {
-        var orders_json = JSON.parse(body);
-        if (orders_json.error) {
-          response.send(orders_json.error);
-          return;
-        }
-        // add each order asynchronously
-        async.forEach(orders_json.orders, addOrder, function(err) {
-          if (err) {
-            console.log(err);
-            response.send("error adding orders");
-          } else {
-            // orders added successfully
-            response.redirect("/orders");
-          }
+        res.on('end', function() {
+            try {
+                var orders_json = JSON.parse(body);
+                if (orders_json.error) {
+                    response.send(orders_json.error);
+                    return;
+                }
+                // add each order asynchronously
+                async.forEach(orders_json.orders, addOrder, function(err) {
+                    if (err) {
+                        console.log(err);
+                        response.send("error adding orders");
+                    } else {
+                        // orders added successfully
+                        response.redirect("/orders");
+                    }
+                });
+            } catch (error) {
+                console.log(error);
+                response.send("error parsing json");
+            }
         });
-      } catch (error) {
-        console.log(error);
-        response.send("error parsing json");
-      }
-    });
 
-    res.on('error', function(e) {
-      console.log(e);
-      response.send("error syncing orders");
+        res.on('error', function(e) {
+            console.log(e);
+            response.send("error syncing orders");
+        });
     });
-  });
 
 });
 
 // sync the database and start the server
 db.sequelize.sync().complete(function(err) {
-  if (err) {
-    throw err;
-  } else {
-    http.createServer(app).listen(app.get('port'), function() {
-      console.log("Listening on " + app.get('port'));
-    });
-  }
+    if (err) {
+        throw err;
+    } else {
+        http.createServer(app).listen(app.get('port'), function() {
+            console.log("Listening on " + app.get('port'));
+        });
+    }
 });
 
 // add order to the database if it doesn't already exist
 var addOrder = function(order_obj, callback) {
-  var order = order_obj.order; // order json from coinbase
-  if (order.status != "completed") {
-    // only add completed orders
-    callback();
-  } else {
-    var Order = global.db.Order;
-    // find if order has already been added to our database
-    Order.find({where: {coinbase_id: order.id}}).success(function(order_instance) {
-      if (order_instance) {
-        // order already exists, do nothing
+    var order = order_obj.order; // order json from coinbase
+    if (order.status != "completed") {
+        // only add completed orders
         callback();
-      } else {
-        // build instance and save
-          var new_order_instance = Order.build({
-          coinbase_id: order.id,
-          amount: order.total_btc.cents / 100000000, // convert satoshis to BTC
-          time: order.created_at
+    } else {
+        var Order = global.db.Order;
+        // find if order has already been added to our database
+        Order.find({where: {coinbase_id: order.id}}).success(function(order_instance) {
+            if (order_instance) {
+                // order already exists, do nothing
+                callback();
+            } else {
+                // build instance and save
+                var new_order_instance = Order.build({
+                    coinbase_id: order.id,
+                    amount: order.total_btc.cents / 100000000, // convert satoshis to BTC
+                    time: order.created_at
+                });
+                new_order_instance.save().success(function() {
+                    callback();
+                }).error(function(err) {
+                    callback(err);
+                });
+            }
         });
-          new_order_instance.save().success(function() {
-          callback();
-        }).error(function(err) {
-          callback(err);
-        });
-      }
-    });
-  }
+    }
 };
